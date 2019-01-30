f51cda211180deed16dc1a0e3e619efeb03c0ff4
Gilbert Torchon <gilbert@noukod.com>
2019-01-30 17:18:57 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,11 @@
 class ProductsController < ApplicationController
  def index
+   @cart=self.cart
  end
 
  def add
-   @cart=self.cart
-   @cart.push(params[:product])
+   cart=self.cart
+   cart.push(params[:product])
    redirect_to products_path
  end
 end
