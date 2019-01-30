e054d482b8c3a6c3bb08be8bfbc35d63d537a9da
Gilbert Torchon <gilbert@noukod.com>
2019-01-30 16:35:33 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def home
+  end
+
   def cart
     @cart=session[:cart] || []
-    return @cart
   end
 end
